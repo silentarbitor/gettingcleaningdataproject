34a9f31562ed70753047ff8e30555d0eb3ef62af
silentarbitor <silentarbitor@gmail.com>
2014-07-27 08:08:44 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,7 @@
 #setwd("/Users/Tsundere/Desktop/Coursera/class 3/")
 run_analysis <- function()
 {
-  
+  # get files from various directories
   setwd("/Users/Tsundere/Desktop/Coursera/class 3/project/test")
   testx <- read.table("X_test.txt")
   testy <- read.table("y_test.txt")
@@ -14,12 +14,15 @@ run_analysis <- function()
   feature <- read.table("features.txt")
   featurenames <- as.vector(feature[,2])
   
+  #names to use as colnames for y
   names <- c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", 
              "SITTING", "STANDING", "LAYING") 
   
+  #combining all the pieces together
   x <- rbind(testx, trainx)
   y <- rbind(testy, trainy)
   
+  #replacing numbers with names
   y[y=="1"] <- names[1]
   y[y=="2"] <- names[2]
   y[y=="3"] <- names[3]
@@ -27,14 +30,11 @@ run_analysis <- function()
   y[y=="5"] <- names[5]
   y[y=="6"] <- names[6]
   
+  #putting the rest together into a giant dataset
   datadump <- cbind(y,x)  
   colnames(datadump) <- c("Activity", featurenames))
   
-
-    #data.frame(as.data.frame(datadump[2])) #setting the first value since when i try to
-  #cbind an empty dataframe with a column from datadump, i get an error and I cant figure out how 
-  #to fix it. 
-  
+  #constructing the tidy dataset
   tidywidy <- as.data.frame(datadump[,1])
   tidycolnames <- vector(mode="numeric", length=0)
 
